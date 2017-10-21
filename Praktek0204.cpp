fbd94c32570708fcb4a7764f7b3645d767936abe
wahyumustajib69 <adjieb.wahyu@gmail.com>
2017-10-22 00:00:17 +0800

    Ngetik sambil banyak banget

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -4,4 +4,21 @@ using namespace std;
 
 int main(){
 	double lurum,par=1.87596,jumbi,bipar,bipas,jumbox,tobil,x,y; 
+	
+	cout<<"LUAS RUMAH\t\t";
+	cin>>lurum;
+	cout<<"Jumlah Box Perket";cin>>jumbox;
+	cout<<"Luas Rumah : "<<lurum;cout<<"m2"<<endl;
+	x=par*jumbi;
+	y=lurum/x;
+	jumbox=y*193/100;
+	cout<<"Jumlah Box Parket:"<<jumbox<<endl;
+	bipar=jumbox*500;
+	cout<<"Biaya Parket (Rp) : "<<bipar<<endl;
+	bipas=lurum*20;
+	cout<<"Biaya Pasang (Rp) : "<<bipas<<endl;
+	tobil=bipar+bipas;
+	cout<<"Total Biaya (Rp) : "<<tobil<<endl;
+	
+		
 }
